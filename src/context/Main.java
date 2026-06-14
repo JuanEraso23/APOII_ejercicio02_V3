3274f68daecd59df199517ec84ba80b12b69da69
Juan Manuel Eraso Grijalba <144852394+JuanEraso23@users.noreply.github.com>
2026-06-14 16:07:07 -0500

    Update Main.java

--- src/context/Main.java
+++ src/context/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Atributos
-		float n1=Float.parseFloat(JOptionPane.showInputDialog("Ingrese la Calificación 1:"));
-        float n2=Float.parseFloat(JOptionPane.showInputDialog("Ingrese la Calificación 2:"));
-        float n3=Float.parseFloat(JOptionPane.showInputDialog("Ingrese la Calificación 3:"));
+		float n1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la Calificación 1:"));
+        float n2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la Calificación 2:"));
+        float n3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la Calificación 3:"));
         
         //Formula
-        float prom=((n1 + n2 + n3) / 3);
+        float prom = ((n1 + n2 + n3) / 3);
         
         //Resultados
         JOptionPane.showMessageDialog(null, "Promedio: " + prom);
